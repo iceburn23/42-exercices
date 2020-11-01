df92c71540de68c09ad02fac369594cf87951c29
Hugo Goncalves <hgoncalv@c2r2s4.42lisboa.com>
2020-11-01 20:34:25 +0000

    a

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,1 +1,50 @@
-iii
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/01 20:33:03 by hgoncalv          #+#    #+#             */
+/*   Updated: 2020/11/01 20:33:07 by hgoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdbool.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		read_index;
+	int		index;
+	bool	swapped;
+
+	read_index = 0;
+	while (true)
+	{
+		swapped = false;
+		index = 0;
+		while (index < size)
+		{
+			if (tab[index] > tab[index + 1])
+			{
+				ft_swap(&tab[index], &tab[index + 1]);
+				swapped = true;
+			}
+			index++;
+		}
+		if (!swapped)
+		{
+			break ;
+		}
+		read_index++;
+	}
+}
