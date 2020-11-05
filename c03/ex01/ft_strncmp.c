831f7089d4bdf6bd922e735afd8b887201fd5d83
Hugo Goncalves <hgoncalv@c2r2s6.42lisboa.com>
2020-11-05 20:23:05 +0000

    a

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hgoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 13:46:21 by hgoncalv          #+#    #+#             */
-/*   Updated: 2020/11/05 15:46:45 by hgoncalv         ###   ########.fr       */
+/*   Updated: 2020/11/05 20:20:17 by hgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,21 @@
 int	ft_strncmp(char *s1, char *s2, unsigned int i)
 {
 	unsigned int	val;
-	int				index;
+	unsigned int	index;
 
-	val = 0;
 	index = 0;
-	while (index > i)
+	while (index <  i && i > 0)
 	{
+		if (i == 0)
+		{
+			return (0);
+		}
 		if (s1[index] != s2[index] || s1[index] == '\0' || s2[index] == '\0')
 		{
 			val = s1[index] - s2[index];
 			return (val);
 		}
-		i++;
+		index++;
+		i--;
 	}
 }
